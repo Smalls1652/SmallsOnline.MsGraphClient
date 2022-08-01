aca1de2eda19978100c8a377f07a633278a7c0b8
Timothy Small <smalls.online@icloud.com>
2022-08-01 15:11:21 -0400

    Added null handling to CheckAuthStatus()

--- src/SmallsOnline.MsGraphClient/models/GraphClient_CheckAuthStatus.cs
+++ src/SmallsOnline.MsGraphClient/models/GraphClient_CheckAuthStatus.cs
@@ -4,6 +4,11 @@ public partial class GraphClient : IGraphClient
 {
     private void CheckAuthStatus()
     {
+        if (_graphClientApp.AuthenticationResult is null)
+        {
+            throw new Exception("AuthenticationResult is null. The GraphClientApp was potentially not connected.");
+        }
+
         DateTimeOffset currentDateTime = DateTimeOffset.Now;
         if (currentDateTime >= _graphClientApp.AuthenticationResult.ExpiresOn)
         {
