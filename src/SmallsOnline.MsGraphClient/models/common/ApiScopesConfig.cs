208b4f67482164b95270215f7c4d220400ee0c40
Timothy Small <smalls.online@icloud.com>
2022-08-01 15:15:02 -0400

    Made Scopes not nullable
    This also changes how ApiScopesConfig is constructed.

--- src/SmallsOnline.MsGraphClient/models/common/ApiScopesConfig.cs
+++ src/SmallsOnline.MsGraphClient/models/common/ApiScopesConfig.cs
@@ -1,10 +1,14 @@
-namespace SmallsOnline.MsGraphClient.Models.Common
+namespace SmallsOnline.MsGraphClient.Models.Common;
+
+/// <summary>
+/// A collection of scopes to use when authenticating to the Microsoft Graph API.
+/// </summary>
+public class ApiScopesConfig
 {
-    /// <summary>
-    /// A collection of scopes to use when authenticating to the Microsoft Graph API.
-    /// </summary>
-    public class ApiScopesConfig
+    public ApiScopesConfig(IEnumerable<string> scopes)
     {
-        public IEnumerable<string> Scopes { get; set; }
+        Scopes = scopes;
     }
+
+    public IEnumerable<string> Scopes { get; set; }
 }
